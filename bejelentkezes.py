cc73f72f67871a3f5e149de21463e1e557eeab27
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2022-11-29 14:05:35 +0100

    Add files via upload

---
+++ bejelentkezes.py
@@ -0,0 +1,8 @@
+jelszó = ['Szivecske<3','bori99']
+while True:
+    nev = input('Adja meg a felhasználónevét: ')
+    jelszo = input('Adja meg a jelszavát: ')
+    if nev == 'bori99' and jelszo == 'Szivecske<3':
+        print('Belépés engedélyezve')
+    else:
+        print('Belépés megtagadva')
