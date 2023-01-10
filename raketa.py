8390cd9308da4c39fc960e4bf24fcd391ec0ab7e
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-01-10 13:56:09 +0100

    Add files via upload

---
+++ raketa.py
@@ -0,0 +1,5 @@
+visszasz = int(input('Hány órás visszaszámlálást tervezünk? '))
+print('Visszaszámlálás: ',visszasz)
+for i in range(visszasz):
+    print('Fell kell függeszteni a visszaszámlálást (i/n)? ')
+    print('Visszaszámlálás: ',visszasz-1)
