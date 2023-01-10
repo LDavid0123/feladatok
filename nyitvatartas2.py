c3e8bff2d2d1cd05459cc334edb2e0a9e7b7e386
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-01-10 13:38:06 +0100

    Add files via upload

---
+++ nyitvatartas2.py
@@ -0,0 +1,7 @@
+ora = int(input('Hány óra van most? '))
+if ora >=8 and ora <= 16:
+    print('A bolt nyitva van. ')
+else: 
+    print('A bolt zárva van. ')
+if ora >=8 and ora <= 16:
+    print('Ennyi órád van még odaérni: ',16-ora)
