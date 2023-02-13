eba94f0994dcb36130b310532324fff02960299e
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-02-13 10:21:42 +0100

    Add files via upload

---
+++ veletlensz.py
@@ -0,0 +1,10 @@
+import random
+
+szam = random.randint(1,3)
+bszam = int(input('Kérlek adj meg egy számot. '))
+if bszam = szam:
+    print('Eltaláltad! ')
+elif bszam > szam:
+    print('Egyel kevesebb a gondolt szam. ')
+elif szam > bszam:
+    print('Egyel több a gondolt szám. ')
