88f87b38c8599838e0e97c940b1ef7130f1b48af
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-02-13 09:47:27 +0100

    Add files via upload

---
+++ gondoltszam.py
@@ -0,0 +1,11 @@
+import random
+
+szam = random.randint(1,5)
+
+bszam = int(input('Kérlek adj meg egy számot. '))
+if bszam = szam:
+    print('A 2 szám egyenlő. ')
+elif bszam > szam:
+    print('Az általad megadott szám nagyobb. ')
+elif szam > bszam:
+    print('A gondolt szám nagyobb. ')
