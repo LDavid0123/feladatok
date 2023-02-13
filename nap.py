4d1bd2df5e0d0c8b9755cc0a565ccc80fb8b0a65
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-02-13 09:13:20 +0100

    Add files via upload

---
+++ nap.py
@@ -0,0 +1,7 @@
+#Készíts egy programot, amely megkérdezi a felhasználótól, hogy jó napja van-e! A válasz kétféle lehet: igen vagy nem. A választól függően írjon ki üzenetet a gép!
+
+valasz = input('Jó napod van? ')
+if valasz = 'igen':
+    print(':)')
+elif valasz = 'nem':
+    print(':(')
---
+++ nap2.py
@@ -0,0 +1,7 @@
+valasz = input('Jó napod van? ')
+if valasz = 'igen':
+    print(':)')
+elif valasz = 'nem':
+    print(':(')
+else:
+    print('Sajnos nem értem a válaszodat!')
