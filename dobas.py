d1d40304e7484d951548bd7279078fde367b8b70
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-02-14 14:02:47 +0100

    Add files via upload

---
+++ dobas.py
@@ -0,0 +1,12 @@
+import random
+
+penz = random.randint(1,2)
+erme = input('Fej vagy írás? ')
+if penz == 1 and erme = Fej:
+    print('Eltaláltad')
+if penz == 2 and erme = írás:
+    print('Eltaláltad')
+if penz == 1 and erme = írás:
+    print('Nem taltaláltad el')
+if penz == 2 and erme = Fej:
+    print('Nem taltaláltad el')
