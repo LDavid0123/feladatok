f8657f238281fd814720219cfd65f1075f5bbf0e
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-02-20 09:28:06 +0100

    Add files via upload

--- feladat.py
+++ feladat.py
@@ -1,58 +1,11 @@
-import urllib.request
-import sys
+def nagyobb(oldal):
+    if oldal1 > oldal2:
+        return 'A könyv oldalszáma az egyik könyvben több,', oldal1
+    elif oldal2 > oldal1:
+        return 'A könyv oldalszáma a másik könyvben több,', oldal2
+    elif oldal2 == oldal1:
+        return 'A két könyv egyenlő oldalszámú.'
 
-def downnload_function(url):
-    try:
-        content = urllib.request.urlopen(url)
-        page_content = content_read()
-        file = open('page_content.txt', 'wb')
-        file.write(page_content)
-        file.close()
-        print(('Content downloaded succesfully'))
-    except:
-        print('Something went wrong')
-
-
-
-import bleach
-
-bleach.clean('an <script>evil()</script> example')
-u'an &lt;script&gt;evil()&lt;/script&gt; example'
-
-bleach.linkify('an http://example.com url')
-u'an <a href="http://example.com" rel="nofollow">http://example.com</a> url'
-
-bleach.clean(
-    "some text",
-    tags=["a", "p", "img"],
-    protocols=["http", "https"],
-)
-
-bleach.linkify(
-    "some text",
-    skip_tags=["pre"],
-)
-
-linker = Linker(
-    skip_tags=["pre"],
-    recognized_tags=html5lib_shim.HTML_TAGS + ["custom-element"],
-)
-
-
-pi = 3.14
-
-def kerulet(r):
-    return 2 * r * pi
-
-def terulet(r):
-    return r ** 2 * pi
-
-
-from kor import kerulet as k_kerulet, terulet as k_terulet
-from negyzet import kerulet, terulet
-
-print(f'Kör kerülete: {kerulet(10):.2f}')
-print(f'Kör területe: {kerulet(10):.2f}')
-
-print(f'Négyzet kerülete: {kerulet(10):.2f}')
-print(f'Négyzet kerülete: {kerulet(10):.2f}')
+oldal1 = int(input('Adj meg az egyik könyv oldalszámát! '))
+oldal2 = int(input('Adjon meg egy másik könyv oldalszámát! '))
+print(nagyobb)
