dfe1c5ca654084839be2aba6052c7716545162e6
LDavid0123 <93654087+LDavid0123@users.noreply.github.com>
2023-03-13 09:24:49 +0100

    Add files via upload

---
+++ py42.py
@@ -0,0 +1,4 @@
+ertelmezesi_tartomany = [-3,4]
+ertek_keszlet = (2*x-3 for x in ertelmezesi_tartomany if x >= 0)
+print('Értelemzési tartomány:', ertelmezesi_tartomany)
+print('Érték készlet:', ertek_keszlet)
---
+++ py43.py
@@ -0,0 +1,4 @@
+import random
+lista = []
+for _ in range(5):
+    lista.append(random.randint(1, 10))
